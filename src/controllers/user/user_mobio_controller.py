feb4278cb1df4579ead99ebbb1e25b629c8fd126
duong <vtduong00116@gmail.com>
2025-05-08 10:30:27 +0700

    test: error 2

--- src/controllers/user/user_mobio_controller.py
+++ src/controllers/user/user_mobio_controller.py
@@ -1,7 +1,7 @@
 import re
 from typing import TypedDict
 
-from flask import Blueprint, Response, jsonify, request
+from flask import Blueprint, jsonify, request
 from mobio.libs.validator import InstanceOf, Length, Required
 from mobio.sdks.base.controllers import BaseController
 
@@ -16,7 +16,7 @@ class UserMobioModel(TypedDict):
 
 @user_bp.route("/user/<int:user_id>", methods=["GET", "POST"])
 class UserMobioController(BaseController):
-    def get(self, user_id: int) -> Response:
+    def get(self, user_id: int):
         # Logic to get user data
         request_data = request.args.get("data", "")
 
@@ -28,12 +28,12 @@ class UserMobioController(BaseController):
             except AttributeError:
                 return jsonify({"error": "Invalid data format"}), 400
 
-        return jsonify({"message": "Get user data"})
+        return jsonify({"message": "Get user data"}), 200
 
     def post(self, user_id: int):
         user_data = request.json
         self._validate_create_item(user_data)
-        return jsonify({"message": "User created", "data": user_data})
+        return jsonify({"message": "User created", "data": user_data}), 201
 
     def _validate_create_item(self, data):
         rules = {
