dd43f80ac92812e100a1a0f5e73d7523a17e8ed8
duong <vtduong00116@gmail.com>
2025-05-08 16:04:42 +0700

    error: errro 5

--- app_journey_builder_api.py
+++ app_journey_builder_api.py
@@ -6,9 +6,13 @@ Date created: 2025/05/05
 
 from src.apis.v1_0.blueprints_api import app
 
+
+def test():
+    d = 0 / 0
+    print(d)
+
+
 print(app.url_map)
 if __name__ == "__main__":
+    test()
     app.run(host="0.0.0.0", port=5000, debug=True)
-
-d = 0 / 0
-print(d)
